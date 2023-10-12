7b183fd0db1857a0105c1794ce1f2d51d0594426
baru-d <barudaniel6@gmail.com>
2023-10-12 06:12:12 +0300

    testing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// javascript for ISS Tracking Site
-function getIssTrackerLocation() {
+// JavaScript for ISS Tracking 
+// Function to fetch ISS location and update details
+function updateIssDetailsAndMap() {
     fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json')
         .then(response => {
             if (!response.ok) {
@@ -8,53 +9,52 @@ function getIssTrackerLocation() {
             return response.json();
         })
         .then(data => {
-            // process the data received from the API
             const issPosition = data.iss_position;
-            const longitude = issPosition.longitude;
-            const latitude = issPosition.latitude;
+            const longitude = parseFloat(issPosition.longitude);
+            const latitude = parseFloat(issPosition.latitude);
 
-            // creating and setting up the map using Leaflet
-            const map = L.map('map').setView([parseFloat(latitude), parseFloat(longitude)], 13);
+            // Create and set up the map using Leaflet
+            const map = L.map('map').setView([latitude, longitude], 13);
+            // zoom control for the map
+            L.control.zoom({ position: 'topright' }).addTo(map);
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+            
 
-            // creating an ISS marker
+            // Create an ISS marker
             const issIcon = L.icon({
                 iconUrl: 'ISS-Tracker/images/iss.png',
                 iconSize: [50, 50],
                 iconAnchor: [25, 25],
             });
-            const issMarker = L.marker([latitude, longitude], { icon: issIcon }).addTo(map);
+
+            L.marker([latitude, longitude], { icon: issIcon }).addTo(map);
+
+            // Update ISS details
+            const details = document.getElementById('details');
+            details.querySelector('.time').textContent = new Date(data.timestamp * 1000).toLocaleString();
+            details.querySelector('.latitude').textContent = latitude;
+            details.querySelector('.longitude').textContent = longitude;
         })
         .catch(error => {
             console.log('Error has occurred', error);
         });
 }
 
-getIssTrackerLocation();
-function updateIssDetails(){
-    fetch('https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json')
-        .then(response=> response.json())
-        .then(data=> {
-            const details = document.getElementById('details');
-            details.querySelector('.time').textContent = new Date(data.timestamp * 1000).toLocaleString();
-            details.querySelector('.latitude').textContent = data.iss_position.latitude;
-            details.querySelector('.longitude').textContent = data.iss_position.longitude;
-        })
-}
-updateIssDetails();
-// getting a reference to the random facts list and toggle button
+// Initial fetch and update
+updateIssDetailsAndMap();
+
+// Getting a reference to the random facts list and toggle button
 const randomFactsList = document.querySelector('#random-facts');
 const toggleFactsButton = document.querySelector('#toggle-facts-button');
 
-// hide random facts initially - initially
+// Hide random facts initially
 randomFactsList.style.display = 'none';
 
-//adding a click event listener to the toggle button
-function toggleRandomFacts(){
-    if(randomFactsList.style.display === 'none'){
+// Adding a click event listener to the toggle button
+function toggleRandomFacts() {
+    if (randomFactsList.style.display === 'none') {
         randomFactsList.style.display = 'block';
-    }
-    else{
+    } else {
         randomFactsList.style.display = 'none';
     }
 }
